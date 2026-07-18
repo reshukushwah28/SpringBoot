00a153036239e15212839f27d0f6a4182bcde7b9
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-07-18 10:54:20 +0530

    CreateUpdate

--- demo/src/main/java/com/reshu/demo/StudentServer/Entity/Student.java
+++ demo/src/main/java/com/reshu/demo/StudentServer/Entity/Student.java
@@ -1,8 +1,11 @@
 package com.reshu.demo.StudentServer.Entity;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 public class Student {
@@ -11,6 +14,12 @@ public class Student {
     String name;
     int age;
     String department;
+
+    @CreationTimestamp
+    LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
 
     public int getId() {
         return id;
@@ -43,5 +52,12 @@ public class Student {
     public void setDepartment(String department) {
         this.department = department;
     }
-}
 
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
