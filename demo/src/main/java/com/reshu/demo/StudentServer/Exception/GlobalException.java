fe53665b4a49ba8226b7af0eceed8952f5197fb1
Reshu Kushwah <143714790+reshukushwah28@users.noreply.github.com>
2026-07-21 11:53:16 +0530

    AddingCheckedExceptional

--- demo/src/main/java/com/reshu/demo/StudentServer/Exception/GlobalException.java
+++ demo/src/main/java/com/reshu/demo/StudentServer/Exception/GlobalException.java
@@ -1,18 +1,26 @@
 package com.reshu.demo.StudentServer.Exception;
 
-import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalException {
+
+    // Handles all RuntimeExceptions
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeExecutionException(RuntimeException e){
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
+
+    // Handles all other Exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(e.getMessage());
     }
 }
